d96ac5e053d3c362e4a64d7b9b20cc6693fd8225
Stephen Lauck <stephen.lauck@gmail.com>
2020-05-17 00:53:37 -0700

    add sprite object

--- basic.cpp
+++ basic.cpp
@@ -1,26 +1,31 @@
-
+// Basic SDL Demo
 #include <SDL2/SDL.h>
 #include <iostream>
 using namespace std;
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-#define SCALE 4
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define SCALE 3
 #define SHEET "img/metroid.bmp"
+
+// PROTOTYPE:
+// SpriteSheet metroid ("img/metroid");
+// Sprite samus ( clip_x, clip_y, clip_w, clip_h );
+// samus.move((SCREEN_WIDTH - samus.w)/2, (SCREEN_HEIGHT - samus.h)/2);
+// samus.draw();
 
 class Sprite {
  private:
-
+  SDL_Rect clip_rect;
+  SDL_Rect draw_rect;
+  
  public:
-  Sprite(){}
-  ~Sprite()
-  {
-    free();
-  }
+  Sprite(int clip_x, int clip_y, int clip_w, int clip_h);
+  ~Sprite();
 
   void free();
-  
-  int draw(int loc_x, int loc_y);
+  void move(int x, int y);
+  int draw();
 };
 
 // Sprite Texture
@@ -28,39 +33,30 @@ SDL_Surface *surface;
 SDL_Texture *texture;
 SDL_Renderer *renderer;
 
+Sprite::Sprite(int clip_x, int clip_y, int clip_w, int clip_h)
+{
+  clip_rect = { clip_x, clip_y, clip_w, clip_h };
+  draw_rect = { (SCREEN_WIDTH - clip_w)/2, (SCREEN_HEIGHT - clip_h)/2, clip_w*SCALE, clip_h*SCALE };
+}
+
+Sprite::~Sprite()
+{
+  free();
+}
 
 void Sprite::free()
 {
   SDL_DestroyTexture(texture);
 }
 
-int Sprite::draw(int loc_x, int loc_y)
+void Sprite::move(int x, int y)
 {
-  //rects for drawing sprite
-  SDL_Rect srcr;
-  SDL_Rect dstr;
+  draw_rect.x = x;
+  draw_rect.y = y;
+}
 
-  // sprite location and size
-  srcr.x = 128;
-  srcr.y = 121;
-  srcr.w = 16;
-  srcr.h = 32;
-
-  // dstr.x = (WINDOW_WIDTH - srcr.w) / 2;
-  // dstr.y = (WINDOW_HEIGHT - srcr.h) / 2;
-
-  dstr.x = loc_x;
-  dstr.y = loc_y;
-
-  dstr.w = srcr.w * SCALE;
-  dstr.h = srcr.h * SCALE;
-
-  surface = SDL_LoadBMP(SHEET);
-  if (!surface) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-    return 3;
-  }
-
+int Sprite::draw()
+{
   SDL_SetColorKey( surface, SDL_TRUE, SDL_MapRGB( surface->format, 0x74, 0x74, 0x74 ) );
   
   texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -68,13 +64,19 @@ int Sprite::draw(int loc_x, int loc_y)
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
     return 3;
   }
-  SDL_FreeSurface(surface);
-
-
-  // draw
   
-  SDL_RenderCopy(renderer, texture, &srcr, &dstr);
+  SDL_RenderCopy(renderer, texture, &clip_rect, &draw_rect);
 
+  return 0;
+}
+
+int loadSheet()
+{
+  surface = SDL_LoadBMP(SHEET);
+  if (!surface) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
+    return 3;
+  }
 
   return 0;
 }
@@ -85,15 +87,15 @@ int main(int argc, char *argv[])
   SDL_Window *window;
   SDL_Event event;
 
-  Sprite samus;
-  Sprite samus2;
+  loadSheet();
+  Sprite samus (128, 121, 16, 32);
   
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize SDL: %s", SDL_GetError());
     return 3;
   }
 
-  if (SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+  if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window nor renderer: %s", SDL_GetError());
     return 3;
   }
@@ -114,12 +116,14 @@ int main(int argc, char *argv[])
     SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0x0 );
     SDL_RenderClear( renderer );
 
-    samus.draw(100, 100);
-    samus2.draw(200, 200);
-
+    // samus.move(100, 100);
+    samus.draw();
+    
     SDL_RenderPresent(renderer);    
   }
 
+
+  SDL_FreeSurface(surface);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
